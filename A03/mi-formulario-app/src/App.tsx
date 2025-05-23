9ab520884a5368ae4dee7847c974dcd4d64ee1cc
juanitoeldesastre <juanitoeldevastador@gmail.com>
2025-05-23 02:22:05 +0000

    Agregar guía práctica 02 y mi-formulario-app en React

---
+++ A03/mi-formulario-app/src/App.tsx
@@ -0,0 +1,12 @@
+import FormularioContacto from './components/FormularioContacto'; 
+ 
+function App() { 
+  return ( 
+    <div style={{ padding: '20px' }}> 
+      <h1>Mi App con React + TypeScript</h1> 
+      <FormularioContacto /> 
+    </div> 
+  ); 
+} 
+ 
+export default App;
